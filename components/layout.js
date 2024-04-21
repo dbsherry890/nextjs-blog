705d73e25cce9458a1cd79bf632a76e04d42ffd2
Daniel B Sherry <dsherry@proxlearn.com>
2024-04-21 15:19:35 -0600

    navbar and css

--- components/layout.js
+++ components/layout.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
+import Navbar from "./navbar";
 
 const name = "Dan Sherry";
 export const siteTitle = "Next.js Website";
@@ -26,6 +27,7 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
+        <Navbar />
         {home ? (
           <>
             <Image
