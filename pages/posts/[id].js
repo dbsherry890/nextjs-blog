a15b4c8ddf142f3909b274f104b3e8d69946527a
Daniel B Sherry <dsherry@proxlearn.com>
2024-04-21 09:27:05 -0600

    add Users and api handling

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,14 @@
+import Date from "../../components/date";
+import Head from "next/head";
 import Layout from "../../components/layout";
+
+import utilStyles from "../../styles/utils.module.css";
 
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  // Fetch necessary data for the blog post using params.id
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -12,6 +17,7 @@ export async function getStaticProps({ params }) {
 }
 
 export async function getStaticPaths() {
+  // Returns a list of possible value for id
   const paths = getAllPostIds();
   return {
     paths,
@@ -22,11 +28,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingL}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
---
+++ pages/users/index.js
@@ -0,0 +1,25 @@
+import styles from "../../styles/Users.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { users: data },
+  };
+};
+
+const Users = ({ users }) => {
+  return (
+    <div className={styles.header}>
+      <h1>Users</h1>
+      {users.map((user) => (
+        <p key={user.id}>
+          <a className={styles.single}>{user.company.catchPhrase}</a>
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Users;
